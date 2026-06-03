37e9d794631679701d17ca3385ed9839c152f0d4
arthurcristofoletti <arthur.cristofoletti@gmail.com>
2026-06-02 22:32:10 -0300

    adjust inputs 2

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
-from src.app import character as personnage
+from src.app import character
 
-def start_player() -> personnage:
-    player_name=input("Name of the character: ")
-    return personnage(player_name)
+def start_player(player_name) -> character:
+    return character(player_name)
 
-def damage_player(player, ammount):
+def damage_player(player: character, ammount: str):
     player.take_damage(int(ammount))
 
-def heal_player(player,ammount):
+def heal_player(player: character, ammount: str):
     player.take_heal(int(ammount))
 
 if __name__ == "__main__":
     print("Starting game....")
-    p1=start_player()
+    player_name=input("Name of the character: ")
+    p1=start_player(player_name)
     ammount=input("Ammount of damage: ")
     damage_player(p1, ammount)
     ammount=input("Ammount of heal: ")
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,9 @@ from src.main import start_player, damage_player, heal_player
 
 def test_main_pipeline_execution():
     """Validates that the main application entry point runs successfully."""
-    player = start_player()
-    damage_player(player)
-    heal_player(player)
+    player = start_player("Test")
+    damage_player(player, "200")
+    heal_player(player, "200")
     assert player.health == 1000
 
 
