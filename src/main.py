906b0918574c9bdb8e76e167d4d6457d54df822f
arthurcristofoletti <arthur.cristofoletti@gmail.com>
2026-06-02 22:28:24 -0300

    adjust inputs

--- src/main.py
+++ src/main.py
@@ -4,18 +4,18 @@ def start_player() -> personnage:
     player_name=input("Name of the character: ")
     return personnage(player_name)
 
-def damage_player(player):
-    ammount=input("Ammount of damage: ")
+def damage_player(player, ammount):
     player.take_damage(int(ammount))
 
-def heal_player(player):
-    ammount=input("Ammount of heal: ")
+def heal_player(player,ammount):
     player.take_heal(int(ammount))
 
 if __name__ == "__main__":
     print("Starting game....")
     p1=start_player()
-    damage_player(p1)
-    heal_player(p1)
+    ammount=input("Ammount of damage: ")
+    damage_player(p1, ammount)
+    ammount=input("Ammount of heal: ")
+    heal_player(p1, ammount)
 
     
