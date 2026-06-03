f93c5cad9dea881c02f293be5df23445e0b1a5f3
arthurcristofoletti <arthur.cristofoletti@gmail.com>
2026-06-02 23:36:53 -0300

    iteration two v1

--- src/app.py
+++ src/app.py
@@ -5,12 +5,27 @@ class character:
         self.level = 1
         self.alive = True
 
+    def get_level(self):
+        return self.level
+    
+    def display_status(self):
+        print(f"Name: {self.name}, Health: {self.health}, Level: {self.level}, Alive: {self.alive}")
+
     def take_damage(self, ammount: int):
         if self.health-ammount <= 0:
             self.alive=False
             self.health=0
         else:
             self.health-=ammount
+
+    def damage_player(self, player, ammount: int):
+        if player != self and player.alive:
+            if player.get_level() >= self.get_level()+5:
+                ammount=int(ammount*0.5)
+            if player.get_level() <= self.get_level()-5:
+                ammount=int(ammount*1.5)
+            player.take_damage(ammount)
+
 
     def take_heal(self, ammount: int):
         if self.alive and ammount >= 0:
@@ -20,3 +35,8 @@ class character:
                self.health+=ammount
         else:
             raise ValueError("Heal impossible")
+    
+    def heal_player(self, player, ammount: int):
+        if player == self:
+            self.take_heal(ammount)
+        
--- src/main.py
+++ src/main.py
@@ -3,18 +3,25 @@ from src.app import character
 def start_player(player_name) -> character:
     return character(player_name)
 
-def damage_player(player: character, ammount: str):
+def damage_player(self, player: character, ammount: str):
     player.take_damage(int(ammount))
 
-def heal_player(player: character, ammount: str):
-    player.take_heal(int(ammount))
+def heal_player(self, ammount: str):
+    self.take_heal(int(ammount))
 
 if __name__ == "__main__":
     print("Starting game....")
     player_name=input("Name of the character: ")
     p1=start_player(player_name)
-    ammount=input("Ammount of damage: ")
-    damage_player(p1, ammount)
-    ammount=input("Ammount of heal: ")
-    heal_player(p1, ammount)
-    print(f"Success: {p1.health}")
+    p1.display_status()
+    player_name=input("Name of the character: ")
+    p2=start_player(player_name)
+    p2.display_status()
+    print("Player 1 attacks player 2")
+    ammount=input("Ammount of damage of player 1 in player 2: ")
+    damage_player(p1, p2, ammount)
+    print("Player 1 successfully attacked player 2")
+    p2.display_status()
+    #ammount=input("Ammount of heal: ")
+    #heal_player(p1, ammount)
+    #print(f"Success: {p1.health}")
